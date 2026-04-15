fa0ab7691967ae7ed1d934c7b470b5033d9baff5
ZucaPersonalizados <willianzucareli@gmail.com>
2026-04-14 22:05:23 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -76,6 +76,7 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const firebaseRuntimeConfig = firebaseConfig;
 
 if (shouldUseEmulators()) {
   connectFirestoreEmulator(db, "127.0.0.1", 8080);
